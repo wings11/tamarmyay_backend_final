2960e426865131ae8a188ce15b9f6a167dc0a2fb
wings11 <wykyaw2001@gmail.com>
2025-06-15 08:18:46 +0700

    edit db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,30 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging: false
+//   }
+// );
+
+// module.exports = { sequelize };
+
 const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'tamarmyay',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'WaiYanKyaw2001',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: false
-  }
-);
-
-module.exports = { sequelize };
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432, 
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For Supabase
+    },
+  },
+});
